05f4bb26b0a90a9a29f8c0c2d4af68a2e308eb39
Rmin <rasooliarmin3@gmail.com>
2022-01-17 04:32:01 +0330

    V2 Is ready  but Back Not ReadY !!

--- app/src/main/java/com/example/login/MainActivity.kt
+++ app/src/main/java/com/example/login/MainActivity.kt
@@ -2,23 +2,27 @@ package com.example.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.EditText
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.pagev2_main)
     }
 
     fun Showinfo(view: android.view.View) {
-        val editTextHello = findViewById(R.id.get_fullname) as EditText
-        Toast.makeText(this,editTextHello.toString(), Toast.LENGTH_LONG).show()
-
+//        val editText = findViewById(R.id.g_fn) as EditText
+//        val showString = editText.text.toString()
+//        Toast.makeText(this, showString, Toast.LENGTH_SHORT).show()
     }
 
-    fun Test(view: android.view.View) {
-
-    }
+    fun register(view: android.view.View) {}
+//    val Full_name = findViewById(R.id.g_fn) as EditText
+//    val Username = findViewById(R.id.g_US) as EditText
+//    val Email = findViewById(R.id.g_EM) as EditText
+//    val PassWord = findViewById(R.id.g_PS) as EditText
+//    val ConfirmPassWord = findViewById(R.id.g_COPS) as EditText
 
 }
